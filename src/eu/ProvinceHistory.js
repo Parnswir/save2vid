6e3c867fba2980474192ee0b186a86bbec58cdc5
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-11-06 19:28:00 +0000

    Implement province history

---
+++ src/eu/ProvinceHistory.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var path = require('path');
+
+var FileReader = require('./ParadoxFileReader');
+
+var Province = function(id, owner) {
+    this.id = id;
+    this.owner = owner;
+};
+
+var ProvinceFactory = function () {};
+
+module.exports = {
+    Province: Province,
+    ProvinceFactory: ProvinceFactory
+};
+
+var ID_REGEXP = new RegExp('(\\d+)', 'g');
+
+ProvinceFactory.prototype.fromFile = function (filePath) {
+    let id = path.basename(filePath).match(ID_REGEXP)[0];
+    return FileReader.fromFile(filePath)
+        .then(function (provinceFile) {
+            let owner = provinceFile.root.owner;
+            return new Province(id, owner || 'NAT');
+        });
+};
+
+ProvinceFactory.prototype.all = function (directory) {
+    let self = this;
+    return Promise.resolve()
+        .then(function () {
+            let files = fs.readdirSync(directory);
+            let promises = files.map(function (file) {
+                let filePath = path.resolve(directory, file);
+                return self.fromFile(filePath);
+            });
+            return Promise.all(promises);
+        });
+};
