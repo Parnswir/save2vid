ce9708cd65fe43fc8d28876b1042b526b145c3ab
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-11-06 12:43:04 +0000

    Implement date utils

--- src/eu/utils.js
+++ src/eu/utils.js
@@ -1,37 +1,27 @@
-define(function() {
+'use strict';
 
-    var isDate = /^\d{3,4}(\.\d{1,2}){2}$/;
+module.exports= {
+    isDate: isDate,
+    parseDate: parseDate
+};
 
-    // Parse a string like "1572.9.22"
-    function parseDate(date) {
-        if(date.constructor === Date) {
-            // Support call with already built date
-            return date;
-        }
-        if(!isDate.test(date)) {
-            throw "Invalid date '"+date+"'";
-        }
+var DATE = /^\d{3,4}(\.\d{1,2}){2}$/;
 
-        var args = date.split(".");
-        var year = parseInt(args[0], 10),
-            month = parseInt(args[1], 10),
-            day = parseInt(args[2], 10);
-        return new Date(year, month, day);
+function isDate(string) {
+    return DATE.test(string);
+}
+
+function parseDate(date) {
+    if (date.constructor === Date) {
+        return date;
+    }
+    if (!isDate(date)) {
+        throw "Invalid date '" + date + "'";
     }
 
-    function contains(elt, array) {
-        for(var size = array.length, i = 0;
-            i < size; i++) {
-            if(elt === array[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    return {
-        isDate: isDate,
-        parseDate: parseDate,
-        contains: contains
-    }
-});
+    let args = date.split(".");
+    let year = parseInt(args[0], 10);
+    let month = parseInt(args[1], 10);
+    let day = parseInt(args[2], 10);
+    return new Date(year, month, day);
+}
